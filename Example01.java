fc7754e648b55c64c799c056b30c0e4e9917f4b8
F-PZ <70877636+F-PZ@users.noreply.github.com>
2020-09-13 20:38:15 +0800

    Update Example01.java

--- Example01.java
+++ Example01.java
@@ -4,20 +4,16 @@ public class Example01 {
 		for (int i=0;i<arr.length;i++){
 			System.out.print(arr[i]+" ");
 		}
-		System.out.println();				//用于换行
-		//2.进行冒泡排序
-		//2.1外层循环定义第一轮需要比较的轮数（两数对比，要比较n-1轮）
+		System.out.println();				
 		for(int i=1;i<arr.length;i++){
-			//2.2内层循环定义第一轮需要比较两个数
 			for(int j=0;j<arr.length-i;j++){
-				if(arr[j]>arr[j+1]){	//比较相邻元素
+				if(arr[j]>arr[j+1]){	
 					int temp=arr[j];
 					arr[j]=arr[j+1];
 					arr[j+1]=temp;
 				}
 			}
 		}
-		//3.完成冒泡排序后，再次循环打印数组元素
 		for(int i=0;i<arr.length;i++){
 			System.out.print(arr[i]+" ");
 		}
