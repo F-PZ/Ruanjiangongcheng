28ea469a94f87ec3e4e147c41a7770f93b3706b1
F-PZ <70877636+F-PZ@users.noreply.github.com>
2020-09-15 18:28:00 +0800

    Update and rename Example01.java to Demo01.java

---
+++ Demo01.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Demo01 {
+ public static void mpPx(int arr[]){
+  for (int i = 0; i <= arr.length - 1; i++){
+   for (int j = 0; j < arr.length - 1 - i;j++){
+    if (arr[j] > arr[j+1]){
+     int temp = arr[j];
+     arr[j] = arr[j+1];
+     arr[j+1] = temp;
+    }
+   }
+  }
+ }
+}
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class Demo01Test {
+
+ @Test
+ public void test() {
+  int[] arr={12,6,4,8,3};
+  Demo01.mpPx(arr);
+  System.out.println(Arrays.toString(arr));
+ }
+}
